21c36fc7d9d3376646fd68b710c1754a1d5a46de
JQK┏ (゜ω゜)=☞ <kangqingjie01@gmail.com>
2018-08-05 14:05:00 +0800

    无序列表 插入法排序

---
+++ insertion_sort.py
@@ -0,0 +1,16 @@
+# coding:utf-8 
+
+def insertion_sort(A):
+	for k in range(1, len(A)):
+		cur = A[k]
+		j = k
+		while j > 0 and A[j-1] > cur:
+			A[j] = A[j-1]
+			j -= 1
+		A[j] = cur
+
+
+if __name__ == '__main__':
+	l = [1,3,2,5,4,6]
+	insertion_sort(l)
+	print(l)
